f4e6db0e7aa332e8c2f74978f76455644ce75587
abdellah711 <alaouiabdellah711@gmail.com>
2023-04-05 13:10:40 +0200

    implement sorting dropdown component

--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -2,19 +2,53 @@
 import type { Product } from '@/types/product';
 import ProductItem from '@/components/product/ProductItem.vue';
 import AddedToCartDialog from './AddedToCartDialog.vue';
-import { ref } from 'vue';
+import SortingDropDown from './SortingDropDown.vue'
+import { computed, ref } from 'vue';
 
-defineProps<{
+const SORT_OPTIONS_CONST = [
+    { label: 'Default', value: 'default' },
+    { label: 'Price', value: 'price' },
+    { label: 'Name (a-z)', value: 'name-az' },
+    { label: 'Name (z-a)', value: 'name-za' },
+
+] as const
+const SORT_OPTIONS = SORT_OPTIONS_CONST as any
+
+type SortValues = (typeof SORT_OPTIONS_CONST[number])['value']
+
+
+const { products } = defineProps<{
     products: Product[]
 }>()
 
 const showAddedToCartDialog = ref(false)
 
+const sortBy = ref<SortValues>('default')
+
+const sortedData = computed(() => {
+    switch (sortBy.value) {
+        case 'default':
+            return products
+        case 'price':
+            return [...products].sort((a, b) => a.price - b.price)
+        case 'name-az':
+            return [...products].sort((a, b) => a.title.localeCompare(b.title))
+            case 'name-za':
+            return [...products].sort((a, b) => b.title.localeCompare(a.title))
+    }
+})
+
+const handleSelect = (str: string) => sortBy.value = str as any
+
 </script>
 
 <template>
+    <div class="flex justify-end">
+        <SortingDropDown :options="SORT_OPTIONS" :selected-value="sortBy" @select="handleSelect" />
+    </div>
     <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <ProductItem v-for="product in products" :key="product.id" :product="product" @add-to-cart="showAddedToCartDialog = true"/>
+        <ProductItem v-for="product in sortedData" :key="product.id" :product="product"
+            @add-to-cart="showAddedToCartDialog = true" />
     </ul>
     <AddedToCartDialog :open="showAddedToCartDialog" @close="showAddedToCartDialog = false" />
 </template>
---
+++ src/components/product/SortingDropDown.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
+import SortIcon from '@heroicons/vue/24/outline/FunnelIcon'
+
+defineProps<{
+    options: { label: string, value: string }[]
+    selectedValue: string
+}>()
+
+defineEmits<{
+    (e:'select',value: string) : void
+}>()
+
+</script>
+<template>
+    <Menu as="div" class="relative inline-block text-left text-zinc-800">
+        <MenuButton class="flex gap-2 items-center bg-white py-2 px-4 rounded-md my-2 shadow-sm">
+            <SortIcon class="w-5" />
+            Sort By
+        </MenuButton>
+        <Transition enter-active-class="transition duration-100 ease-out" enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
+            <MenuItems
+                class="absolute right-0 z-50 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }" v-for="option in options" :key="option.value">
+                    <button class="w-full rounded-md p-2" :class="{ 'bg-zinc-100': active || selectedValue === option.value }" @click="$emit('select',option.value)">
+                        {{option.label}}
+                    </button>
+                    </MenuItem>
+                </div>
+            </MenuItems>
+        </Transition>
+    </Menu>
+</template>
